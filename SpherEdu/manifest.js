f0e7326d38dc4e87615e45b6edd4f170d78c8b27
Otavio322 <93688360+Otavio322@users.noreply.github.com>
2026-06-18 00:37:32 -0300

    Adicionando o Manifest

---
+++ SpherEdu/manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "SpherEdu",
+  "short_name": "SpherEdu",
+  "description": "Sistema de gerenciamento de atividades complementares",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#2196f3",
+  "icons": [
+    {
+      "src": "./icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "./icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
